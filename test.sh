6650a670ef2d9263f099af3d529d462d3f2419ee
Raphael Paiva <raphaeldpaiva@gmail.com>
2017-02-08 18:05:06 -0200

    Adicionando script para rodar os testes.

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set +e
+
+TEST_PATTERN="test_*.py"
+INIT_PATTERN="*__init__*"
+
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    PATH=$PATH:/c/Python27
+fi
+
+rm -f .coverage
+
+for f in `git ls-files $TEST_PATTERN | sed 's/\//\./g' | sed 's/\.py//g'`; do
+  echo "-- $f"
+  python -m coverage run --omit=$TEST_PATTERN,$INIT_PATTERN -a -m $f
+done
+
+python -m coverage html
+
+#python -m unittest `ls *_tests.py | sed 's/\.py//g'`
